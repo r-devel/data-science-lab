bdadcaca8ef494cbdf9e84a8b1ced7cab726c6f7
EllaKaye <ella.kaye@gmail.com>
2026-03-31 11:06:00 +0100

    Viewing stem source code

--- stem.R
+++ stem.R
@@ -3,3 +3,11 @@ a <- c(8.48, 9.58, 9.96)
 stem(a)
 stem(c(2, 2))
 stem(2)
+
+# See source code (note, no parenthesess)
+stem
+
+# In Positron, see the function definition in the source code
+View(stem)
+
+browseURL("https://bugs.r-project.org/show_bug.cgi?id=8934")
